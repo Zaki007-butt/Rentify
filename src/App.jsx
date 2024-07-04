c5534717ebb36ea6fea55dbb1c6438a3e4d1d906
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-04 13:48:37 -0700

    Add tailwind css and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import Navbar from "./components/shared/Navbar"
+
 function App() {
   return (
-    <h1>Welcome to real estate</h1>
+    <>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   )
 }
 
