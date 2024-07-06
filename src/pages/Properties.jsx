950d296a47006b2af9d61e1666607a7ec8c7eaa4
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-06 15:40:20 -0700

    Add selector dropdown UI

--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropertyCard from '../components/cards/PropertyCard'
 import { useGetProperties } from '../react-query/queries/property.queries'
+import Dropdown from '../components/shared/Dropdown'
 
 const Properties = () => {
   const { data: response, isPending } = useGetProperties()
@@ -10,22 +11,28 @@ const Properties = () => {
   }
 
   return (
-    <div class="container mx-auto grid w-full grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-32">
-      {
-        response.data.results.length > 0 && response.data.results.map(property => (
-          <PropertyCard
-            id={property.id}
-            title={property.title}
-            description={property.description}
-            price={property.price}
-            address={property.address}
-            bedroom={property.bedroom}
-            washroom={property.washroom}
-            area={property.area}
-            property_category_name={property.property_category_name}
-          />
-        ))
-      }
+    <div className='container mx-auto'>
+      <div className="flex">
+        <Dropdown filterField="Category"/>
+        <Dropdown filterField="Type"/>
+      </div>
+      <div class="grid w-full grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {
+          response.data.results.length > 0 && response.data.results.map(property => (
+            <PropertyCard
+              id={property.id}
+              title={property.title}
+              description={property.description}
+              price={property.price}
+              address={property.address}
+              bedroom={property.bedroom}
+              washroom={property.washroom}
+              area={property.area}
+              property_category_name={property.property_category_name}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
