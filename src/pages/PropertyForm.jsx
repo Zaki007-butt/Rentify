bd6fcbcc9a87d8fbba1446cd7eb64bc5fe3570f8
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-06 22:02:45 -0700

    Button styling change

--- src/pages/PropertyForm.jsx
+++ src/pages/PropertyForm.jsx
@@ -146,9 +146,9 @@ const PropertyForm = () => {
 
         <button
           type="submit"
-          className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="mt-4 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Submit
+          Add Property
         </button>
       </form>
     </div>
