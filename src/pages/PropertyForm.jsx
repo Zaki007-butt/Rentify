ba03d65a5be1f0ac1681583be6128ba3798c4c09
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-06 22:05:22 -0700

    Change row

--- src/pages/PropertyForm.jsx
+++ src/pages/PropertyForm.jsx
@@ -45,6 +45,7 @@ const PropertyForm = () => {
           {...register('description', { required: 'Description is required' })}
           className="block w-full p-4 mt-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Description"
+          rows={6}
         />
         {errors.description && <span className="text-red-500">{errors.description.message}</span>}
 
