1e7127a0c3de5a8161702919f5c1b798290f684f
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-08 17:44:46 -0700

    Add options to remove filters

--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropertyCard from '../components/cards/PropertyCard'
 import {
   useGetProperties,
@@ -51,6 +51,23 @@ const Properties = () => {
     setSearchParams({ "search": search })
   }
 
+  const clearCategoryFilter = () => {
+    setSearchParams(prevParams => {
+      prevParams.delete('category_id')
+      return prevParams
+    })
+    setCategoryID('')
+    setSubcategoryID('')
+  }
+
+  const clearSubcategoryFilter = () => {
+    setSearchParams(prevParams => {
+      prevParams.delete('subcategory_id')
+      return prevParams
+    })
+    setSubcategoryID('')
+  }
+
   return (
     <div className='container mx-auto'>
       <div className="flex items-center justify-between w-full">
@@ -67,6 +84,28 @@ const Properties = () => {
           />}
         </div>
         <SearchBar searchKeyword={searchKeyword} updateSearchKeyword={updateSearchKeyword} />
+      </div>
+      <div className='flex pb-8'>
+        {categoryID && (
+          <div className="flex items-center space-x-2 mr-2">
+            <span className="font-semibold">{categories?.find(cat => cat.id === parseInt(categoryID))?.name}</span>
+            <button onClick={clearCategoryFilter}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-500 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 1a9 9 0 110 18 9 9 0 010-18zm3.293 5.293a1 1 0 10-1.414-1.414L10 8.586 7.121 5.707a1 1 0 00-1.414 1.414L8.586 10l-2.879 2.879a1 1 0 101.414 1.414L10 11.414l2.879 2.879a1 1 0 101.414-1.414L11.414 10l2.879-2.879z" clipRule="evenodd" />
+              </svg>
+            </button>
+          </div>
+        )}
+        {subcategoryID && (
+          <div className="flex items-center space-x-2">
+            <span className="font-semibold">{subcategories?.find(subcat => subcat.id === parseInt(subcategoryID))?.name}</span>
+            <button onClick={clearSubcategoryFilter}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-500 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 1a9 9 0 110 18 9 9 0 010-18zm3.293 5.293a1 1 0 10-1.414-1.414L10 8.586 7.121 5.707a1 1 0 00-1.414 1.414L8.586 10l-2.879 2.879a1 1 0 101.414 1.414L10 11.414l2.879 2.879a1 1 0 101.414-1.414L11.414 10l2.879-2.879z" clipRule="evenodd" />
+              </svg>
+            </button>
+          </div>
+        )}
       </div>
       <div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {
