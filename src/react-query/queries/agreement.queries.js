094d6c2233a68fb5a1539bfb6e39ce1c67a0c2a4
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2024-11-24 01:54:18 +0500

    Update agreement.queries.js

--- src/react-query/queries/agreement.queries.js
+++ src/react-query/queries/agreement.queries.js
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAgreements, getSingleAgreement } from "../../apis/agreement.api";
 import { QUERY_KEYS } from "../constants/keys";
+import { useAuth } from "../../hooks/useAuth";
 
 export const useGetAgreements = (pageSize = 100) => {
+  const { user } = useAuth();
+
   return useQuery({
-    queryKey: [QUERY_KEYS.AGREEMENTS],
+    queryKey: [QUERY_KEYS.AGREEMENTS, user.user_id],
     queryFn: () => getAgreements(pageSize),
     staleTime: 20 * 1000,
   });
