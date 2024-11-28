8dd2d032d27049a017d0731a1a13e05c619f1119
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2024-11-29 01:38:53 +0500

    Fix user_id issue

--- src/react-query/queries/customer.queries.js
+++ src/react-query/queries/customer.queries.js
@@ -7,8 +7,9 @@ export const useGetCustomer = () => {
   const { user } = useAuth();
 
   return useQuery({
-    queryKey: [QUERY_KEYS.GET_CUSTOMER, user.user_id],
+    queryKey: [QUERY_KEYS.GET_CUSTOMER, user?.user_id],
     queryFn: getCustomer,
     staleTime: 500 * 1000,
+    enabled: !!user?.user_id // Only run query if user_id exists
   });
 };
