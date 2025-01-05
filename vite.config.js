e714661b968b0e2e43adc4f85f3e31da35e81aaa
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2025-01-05 22:39:58 +0500

    Setup deployment 2.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const vitConfig = {
+  plugins: [react()],
+};
+
+if (process.env.NODE_ENV === "production") {
+  vitConfig.base = "/RealEstate-Frontend";
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(vitConfig);
